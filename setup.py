39938c77f96bf379dce1594d6a26b354c2953bb4
w6ye <w6ye@ucsd.edu>
2019-05-10 15:10:58 -0700

    first commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='weike_util',
+      version='0.1',
+      description='Utility codes',
+      url='',
+      author='Weike Ye',
+      author_email='w6ye@ucsd.edu',
+      license='MIT',
+      packages=['weike_util'],
+      zip_safe=False)
